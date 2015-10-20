0c00bf97ba69fd7139ecf0cebefdf06f74ddf10e
James Ross <jamr2@pdx.edu>
2015-10-20 13:31:10 +0000

    cleaned out a couple files from some cpp stuff i did

--- sedhead.h
+++ sedhead.h
@@ -1,7 +1,8 @@
 /*
     stuff i use for stuff.
 
-
+    TODO: find out what people actually prefer in industry, particularly with
+          kernal or systems programming
     I really want to know how i should handle increments in for loops in 
     my macros.
         Have them pass an increment variable?
@@ -9,16 +10,26 @@
         Have the mixed declaration and declare it with random __'s.
 */
 
+#ifndef _SED_HEAD_
+#define _SED_HEAD_
 #include <sys/types.h>
+#include <stdio.h>                  
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <getopt.h>
+#include <time.h>
+#include <ctype.h>
 
+
+/* something is wrong this the following ifdef's. Havent worked it out yet.
+   Trying to get the to work with the hash_table.c program */
 #ifdef __SED_ERR__
     #include "err_handle/err_handle.h"  /* error handling functions */
 #endif
-#undef __SED_ERR__
 #ifdef __SED_NUM__
     #include "get_num/get_num.h"        /* convert strings to int types */
 #endif
-#undef __SED_NUM__
 
 #ifdef __SED_LINUX__
     #include <sys/stat.h>
@@ -27,16 +38,6 @@
     #include <sys/time.h>
 #endif
 
-#ifndef _SED_HEAD_
-#define _SED_HEAD_
-#include <stdio.h>                  
-#include <stdlib.h>
-#include <string.h>
-#include <inttypes.h>
-#include <getopt.h>
-#include <time.h>
-#include <ctype.h>
-#include <assert.h>
 
 #ifndef __FL_CONSTS__
 #define __FL_CONSTS__
